b56cb2c9b7d4b3984c6403e7c45f5972d04f17d4
Aritra Basu <aritrabasu1999@gmail.com>
2018-11-05 01:10:34 +0530

    Adds recaptcha

--- js/submission.js
+++ js/submission.js
@@ -1,31 +1,53 @@
-$("#gform").submit(function(e) {
-        e.preventDefault();
-        $(".contact-submit").html("sending...");
-        $(".contact-submit").prop("disabled", true);
-        if($("#honeypot").val()) return;
-        var formData = $('.form-input').serialize();
-        $.post("https://script.google.com/macros/s/AKfycby6gAujhzOIQCPmBcAu2Kq2XooD87r0OeUoes4jRGlpxeFnhOs/exec", formData)
-        .done(function(data) {
-            if(data.result === "success")
-                toast("Your message has been sent",true);
-            else
-                toast("Couldn't send your message :(", false);
-            $(".contact-submit").html("submit");
-            $(".contact-submit").prop("disabled", false);
-        });
-    });
+var onSuccess = function(e) {
 
-$('.btn-close').on('click', function(e) {
-    $('#message').css('display','none');
+    if(grecaptcha === undefined){
+        alert("No grecaptcha");
+        return;
+    }
+    var response = grecaptcha.getResponse();
+    console.log(response);
+    if (!response) {
+        alert("No response");
+        return;
+    }
+    $(".contact-submit").html("sending...");
+    $(".contact-submit").prop("disabled", true);
+    if($("#honeypot").val()) return;
+    var formData = $('.form-input').serialize();
+    console.log($('.form-input').filter(function(){return !$(this).val()}).length);
+    if ($('.form-input').filter(function(){return !$(this).val()}).length > 0) {
+            toast("fields can't be empty", false);
+            $(".contact-submit").html("submit");
+            return;
+    }
+    $.post("https://script.google.com/macros/s/AKfycby6gAujhzOIQCPmBcAu2Kq2XooD87r0OeUoes4jRGlpxeFnhOs/exec", formData)
+    .done(function(data) {
+        if(data.result === "success")
+            toast("Your message has been sent",true);
+        else
+            toast("Couldn't send your message :(", false);
+        $(".contact-submit").html("submit");
+        $('.form-input').each(function() {
+           $(this).val("");   
+       });
+    }).fail(function (e) {
+        toast("Couldn't send your message :(", false);
+    });
+    grecaptcha.reset();
+};
+
+$('#toast-close').on('click', function(e) {
+    $('#message').fadeOut();
+    $(".contact-submit").prop("disabled", false);
 });
 
 function toast(m,v) {
     $("#message p:first-child").html(m);
     $("#message").css('display','block');
     if (v) {
-        $("#message").addClass('success')
+        $("#message").addClass('success');
     }
     else{
-        $("#message").addClass('error')
+        $("#message").addClass('error');
     }
 }
